01046720f92eed144e465e5c09d3275feacdc745
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-19 14:14:35 +0600

    fully Impliment Cashout section

--- script/cashout.js
+++ script/cashout.js
@@ -1,3 +1,25 @@
 document.getElementById("cashout-btn").addEventListener("click",function () {
-    
+    const cashoutNumber = getValueFromInput("cashout-number");
+    if(cashoutNumber.length != 11){
+        return;
+    }
+    const cashoutAmount = getValueFromInput("cashout-amount");
+
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    const newBalance = Number(balance) - Number(cashoutAmount);
+
+    if(newBalance < 0){
+        alert("Invalid Amount");
+        return
+    }
+    const cashOutPin = getValueFromInput("pin-cashout");
+
+    if(cashOutPin === "1122"){
+        alert("CashOut Successful");
+        balanceElement.innerText = newBalance;
+    }else {
+        alert("Invalid pin");
+        return;
+    }
 })
